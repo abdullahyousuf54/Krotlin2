6f5124a31308ba395ea66cc146ed7943ca2019e3
Abdullah Yousuf <abdullahyousuf54@gmail.com>
2024-06-06 13:08:34 +0530

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var x: Int = 3
+    println("The value of x is $x")
+    x=4
+    println("The value of x is now $x")
+}
